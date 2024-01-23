9743465bc7ac9b4c4c3007e2a4c8978dc694de56
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-01-23 10:50:20 +0530

    Delete Lab2 directory

--- Lab2/q1.py
+++
@@ -1,8 +0,0 @@
-a, b = 0, 1
-max_fib = 100
-
-while a <= max_fib:
-    print(a, end=' ')
-    a, b = b, a + b
-else:
-    print("\nThe Fibonacci sequence has ended.")
--- Lab2/q2.py
+++
@@ -1,6 +0,0 @@
-list1 = [1,-10,5,-11,34,-70]
-i = 0
-while(i<len(list1)):
-	if(list1[i]<0):
-		print(list1[i])
-	i+=1
