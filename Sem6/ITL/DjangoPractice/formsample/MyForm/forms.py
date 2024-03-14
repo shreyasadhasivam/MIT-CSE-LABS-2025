ef67f76c3ebc26f8f9d49b35085d535c5c49b026
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-03-14 10:42:47 +0530

    formsample uploaded

---
+++ Sem6/ITL/DjangoPractice/formsample/MyForm/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class InputForm(forms.Form):
+    first_name = forms.CharField(max_length = 200)
+    last_name = forms.CharField(max_length=200)
+    roll_num = forms.IntegerField(
+        help_text="Enter 6 digit reg number"
+    )
+    password = forms.CharField(widget = forms.PasswordInput())
